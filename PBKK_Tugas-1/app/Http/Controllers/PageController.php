b1356b4ca032cda66b88dde14cea47ee89bb452a
Lilium <scientificwitches@gmail.com>
2026-09-07 18:19:50 +0700

    Modify PageController with new project info
    Updated project details and team member names in PageController.

--- PBKK_Tugas-1/app/Http/Controllers/PageController.php
+++ PBKK_Tugas-1/app/Http/Controllers/PageController.php
@@ -7,8 +7,8 @@ class PageController
     public function index()
     {
         $data = [
-            'nama'  => 'Lorem Ipsum',   
-            'nrp'   => '5025xxxxxx',          
+            'nama'  => 'Fathiya Nayla Husna Wibowo',   
+            'nrp'   => '5025241204',          
             'kelas' => 'PBKK A',               
             'kelompok' => '4',                 
         ];
@@ -23,7 +23,7 @@ class PageController
             'nama_departemen' => 'Departemen Teknik Informatika',
             'institusi'       => 'Institut Teknologi Sepuluh Nopember (ITS)',
             'fakultas'        => 'Fakultas Teknologi Elektro dan Informatika Cerdas (FTEIC)',
-            'deskripsi'       => 'Lorem Ipsum',
+            'deskripsi'       => 'Departemen Teknik Informatika di ITS berdedikasi pada keunggulan dalam pendidikan, penelitian, dan inovasi di bidang ilmu komputer dan rekayasa perangkat lunak. Kami mempersiapkan mahasiswa untuk menjadi pemimpin teknologi melalui kurikulum mutakhir dan fasilitas kelas dunia.​',
         ];
 
         return view('about', $profil);
@@ -33,13 +33,16 @@ class PageController
     public function project()
     {
         $project = [
-            'judul_ide'   => 'Lorem Ipsum', 
-            'sub_tema'    => 'Lorem Ipsum',
-            'deskripsi'   => 'Lorem Ipsum.',
+            'judul_ide'   => 'Pengembangan Agentic AI sebagai Dynamic Application Security Testing (DAST)', 
+            'sub_tema'    => 'Analisis Kerentanan dan Keamanan Aplikasi Web',
+            'deskripsi'   => 'Mengembangkan sistem DAST berbasis Agentic AI yang mampu melakukan reconnaissance, menganalisis endpoint dan form, mengirim payload keamanan secara dinamis, serta mengevaluasi respons HTTP untuk mendeteksi kerentanan seperti SQL Injection dan XSS. Sistem menggunakan LLM dengan tools berbasis Python dan reasoning loop ReAct untuk mensimulasikan proses pentesting secara otonom, kemudian menghasilkan laporan kerentanan beserta rekomendasi perbaikan. Pengujian dilakukan secara lokal menggunakan aplikasi yang memang dirancang untuk pengujian keamanan seperti DVWA atau OWASP Juice Shop.',
             'anggota_kelompok' => [
-                'Nama Anggota 1',
-                'Nama Anggota 2',
-                'Nama Anggota 3',
+                'Kamal Zaky Adinata',
+                'Angela Vania Sugiyono',
+                'Adrian Afzal Zaidana',
+                'Rhea Debora Sianturi',
+                'Fathiya Nayla Husna Wibowo',
+                'Shifa Alya Dewi',
             ],
         ];
 
